5396781a3c6f23c3e3812a51af1e8f00ec6d1065
dkdam <dkdam@live.com>
2022-12-03 23:45:19 +1100

    remove minimum solved

--- removeMinumum/removeMinimum.js
+++ removeMinumum/removeMinimum.js
@@ -1,5 +1,13 @@
-const removeSmallest = () => {
-
+const removeSmallest = (number) => {
+    let low = { "num": Infinity, 
+                "idx": 0 }
+    for(let i = 0; i < number.length; i++){
+        if(number[i] < low["num"]){
+            low["num"] = number[i]
+            low["idx"] = i
+        }
+    }
+    return number.slice(0,low["idx"]).concat(number.slice(low["idx"] + 1)) 
 }
 
 module.exports = removeSmallest
