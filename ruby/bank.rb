b96b55df0fe18e93eccc137d2d0bc18df905fc11
dkdam <dkdam@live.com>
2023-04-24 14:59:41 +1000

    created ruby TDD

---
+++ ruby/bank.rb
@@ -0,0 +1,12 @@
+class Bank
+
+    attr_reader :accounts
+    def initialize(name)
+        @name = name
+        @accounts = {}
+    end
+
+    def create_account(name, balance)
+        @accounts[name] = balance
+    end
+end
---
+++ ruby/spec/bank_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../bank'
+
+describe Bank do
+    let (:bank) { Bank.new( 'TDD Bank' ) }
+    describe ".new" do
+        it "Creates a new bank object" do
+            expect( bank ).to_not eq nil
+        end
+    end
+
+    describe "#create_account" do
+        it "creates a new account for a user with a given starting balance" do
+            bank.create_account 'Bob', 200
+            expect(bank.accounts['Bob']).to eq 200
+        end
+    end
+end
